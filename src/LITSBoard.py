44be58d280aa9088c5886b7090cda99866853c7b
Anders Olsen <aolsen0@mit.edu>
2024-07-14 01:54:31 -0400

    implement validity check

--- src/LITSBoard.py
+++ src/LITSBoard.py
@@ -1,6 +1,10 @@
+import collections
+import itertools
 import random
 import torch
 import torch.nn.functional as F
+
+from src import piece_utils
 
 
 class LITSBoard:
@@ -18,6 +22,8 @@ class LITSBoard:
             max_pieces_per_shape: Maximum number of pieces of each shape that can be
                 placed on the board.
         """
+        if board_size != 10:
+            raise NotImplementedError("board sizes other than 10 not supported")
         if 2 * num_xs > board_size**2:
             raise ValueError("cannot fit that many symbols on the board")
         self.played_ids = []
@@ -44,8 +50,61 @@ class LITSBoard:
     def to_tensor(self) -> torch.Tensor:
         return self._tensor
 
-    def is_valid(self, piece_id: int) -> bool:
-        pass
+    def is_valid(self, new_piece_id: int) -> bool:
+        """Check if a piece can be legally played on the current board."""
+        new_piece_type = piece_utils.get_piece_type_of_id(new_piece_id)
+
+        # Check if we have pieces of this type remaining
+        type_counts = collections.Counter(
+            map(piece_utils.get_piece_type_of_id, self.played_ids)
+        )
+        if type_counts[new_piece_type] >= self.max_pieces_per_shape:
+            return False
+
+        piece_list = piece_utils.build_piece_list()
+        new_cells = piece_list[new_piece_id]
+        # Check that the piece does not intersect already played pieces
+        played_cells = set(
+            itertools.chain(*[piece_list[played_id] for played_id in self.played_ids])
+        )
+        if played_cells & set(new_cells):
+            return False
+
+        # Check that the piece is adjacent to a previously played piece
+        for cell in played_cells:
+            for new_cell in new_cells:
+                if piece_utils.taxi_distance(cell, new_cell) == 1:
+                    break
+            else:
+                continue
+            break
+        else:
+            return False
+
+        # Check that the piece is not adjacent to another piece of the same type
+        for played_id in self.played_ids:
+            if piece_utils.get_piece_type_of_id(played_id) == new_piece_type:
+                for new_cell in new_cells:
+                    for cell in piece_list[played_id]:
+                        distance = piece_utils.taxi_distance(cell, new_cell)
+                        assert distance != 0
+                        if distance == 1:
+                            return False
+
+        # Check that the piece does not fill a 2x2 square anywhere on the board
+        total_cells = set(new_cells) | played_cells
+        for cell in new_cells:
+            row, col = cell
+            # check each of the four possible 2x2 squares containing the cell
+            for row_change in (-1, 1):
+                for col_change in (-1, 1):
+                    if (
+                        (row + row_change, col) in total_cells
+                        and (row, col + col_change) in total_cells
+                        and (row + row_change, col + col_change) in total_cells
+                    ):
+                        return False
+        return True
 
     def valid_moves(self) -> list[int]:
         pass
